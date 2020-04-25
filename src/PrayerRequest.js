33b84889149b6145a862977705507c98f2714311
Nick Little <n14little@gmail.com>
2020-04-24 23:40:07 -0500

    speak prayer requests

--- src/PrayerRequest.js
+++ src/PrayerRequest.js
@@ -1,5 +1,16 @@
 import React, { useState, useEffect } from 'react';
 
+// not all browsers support this
+const synth = window.speechSynthesis;
+const voices = synth.getVoices();
+const americanEnglish = voices[27];
+
+function speakPrayerRequest(pr) {
+  const utterThis = new SpeechSynthesisUtterance(pr.title + pr.request);
+  utterThis.voice = americanEnglish;
+  synth.speak(utterThis);
+}
+
 function PrayerRequest({ initialSelectedPrId }) {
   const [prayerRequests, setPrayerRequests] = useState();
   const [indexOfSelectedPr, setIndexOfSelectedPr] = useState(initialSelectedPrId || 0);
@@ -22,10 +33,12 @@ function PrayerRequest({ initialSelectedPrId }) {
     setIndexOfSelectedPr(nextIndex);
   };
 
+  const playClicked = () => speakPrayerRequest(selectedPrayerRequest);
   return prayerRequests ? (
     <>
       <h2>{selectedPrayerRequest.title}</h2>
       <p>{selectedPrayerRequest.request}</p>
+      <button onClick={playClicked}>Play</button>
       <button onClick={onPrevious}>Previous</button>
       <button onClick={onNext}>Next</button>
     </>
